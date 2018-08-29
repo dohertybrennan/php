425e2bfe1b6538045f4d4b492768e32b9fa274f3
dohertybrennan <dohertybrennan@yahoo.com>
2018-08-29 11:35:59 -0400

    Created login.php and created basic form

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login Page</title>
+  </head>
+  <body>
+    <form>
+      <input type="text"><br />
+      <input type="text">
+    </form>
+  </body>
+</html>
