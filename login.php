51d0c8e2063b2365b6b56140382066d4782dd266
dohertybrennan <dohertybrennan@yahoo.com>
2018-08-30 10:15:13 -0400

    added submit button

--- login.php
+++ login.php
@@ -6,9 +6,9 @@
   </head>
   <body>
     <form method="get" action="">
-      <input type="text" name="first"><br />
-      <input type="text" name="last">
-      <input type="button" name="submit" value="Submit">
+      <input type="text" name="first" value="First" placeholder="First Name"><br />
+      <input type="text" name="last" value="Last" placeholder="Last Name"><br />
+      <input type="submit" name="submit" value="Go! Go! Power Rangers!">
     </form>
   </body>
 </html>
