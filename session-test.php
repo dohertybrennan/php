5e2656e28afd7d46d27e2245bd2616614fee9897
dohertybrennan <dohertybrennan@yahoo.com>
2018-09-04 10:46:38 -0400

    added session-test

---
+++ session-test.php
@@ -0,0 +1,11 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    You are logged in as <?php echo $_SESSION['username']; ?>
+  </body>
+</html>
