25a848cac898757b08716323ee9283a818e9f9fb
dohertybrennan <dohertybrennan@yahoo.com>
2018-09-05 11:34:56 -0400

    added db connection

---
+++ dbconnection.php
@@ -0,0 +1,12 @@
+<?php
+  $db_host = 'localhost';
+  $db_user = 'brennan';
+  $db_password = 'southhills#';
+  $db_name = 'Brennan';
+  $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+  if ($con->connection_error) {
+    die("Connection failed! ". $conn->connection_error);
+  } else {
+    echo "Connection Successful";
+  }
+?>
