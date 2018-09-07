6d44399554ed603cfedfdcc8d76ca8b06180aeb5
dohertybrennan <dohertybrennan@yahoo.com>
2018-09-07 10:15:46 -0400

    big fix

--- dbconnection.php
+++ dbconnection.php
@@ -2,7 +2,7 @@
   $db_host = 'localhost';
   $db_user = 'brennan';
   $db_password = 'southhills#';
-  $db_name = 'Brennan';
+  $db_name = 'brennan';
   $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
   if ($con->connection_error) {
     die("Connection failed! ". $conn->connection_error);
