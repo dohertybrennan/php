d63a1dc43fbef01f83efb725758ed753ffd400ad
dohertybrennan <dohertybrennan@yahoo.com>
2018-09-12 11:22:18 -0400

    bug fixes

--- upload.php
+++ upload.php
@@ -5,11 +5,23 @@
 
   if (!isset($_SESSION['username'])) {
     //die("Don't even think about it");
-    header('login.php');
+    header('Location: login.php');
+  }
+
+  var_dump($_POST['upload']);
+  echo "<hr>";
+  var_dump($_FILES['upload']);
+
+  if ($_FILES['upload'] != null) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir.basename($_FILES['upload']['name']);
+    move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
   }
 ?>
 
-Upload you file.
-<form action="" method="post">
+Upload your file.
+<form action="" method="post" enctype="multipart/form-data">
   <input type="file" name="upload">
+  <br>
+  <input type="submit" name="submit">
 </form>
