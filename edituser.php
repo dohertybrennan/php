97c8d73f8b69c8437943fe167d81494596b8bdb9
dohertybrennan <dohertybrennan@yahoo.com>
2018-09-26 10:54:26 -0400

    added edit functionality

--- edituser.php
+++ edituser.php
@@ -9,18 +9,26 @@
         header('Location: login.php');
     }
 
+    if (isset($_POST['submit'])) {
+        $sql = "UPDATE users SET username = \" $_POST['username']\", password = \" $_POST['password'] \" WHERE user_id = $_POST['id']";
+        $result = $conn -> query($sql);
+    }
+
     if (isset($_POST['id']) && $_POST['edit'] == 'Edit') {
         require('dbconnection.php');
         $sql = "SELECT * FROM users WHERE user_id = ". $_POST['id'];
         $result = $conn->query($sql);
 
-        echo "<form action=\"\" method=\"post\">";
+        echo "<form action='' method='post'>";
 
         while ($row = $result->fetch_assoc()) {
-            echo "<input type=\"text\" disabled value='" . $row['user_id'] . "'><br>";
-            echo "<input type=\"text\" value='" . $row['user_id'] . "'><br>";
-            echo "<input type=\"text\" value='" . $row['password'] . "'>";
+            echo "<input type=\"text\" disabled value='" . $row['user_id'] . "' name='id'><br>";
+            echo "<input type=\"text\" value='" . $row['username'] . "' name='username'><br>";
+            echo "<input type=\"text\" value='" . $row['password'] . "' name='password'>";
         }
+
+        echo "<button type=\"submit\" value=\"Save Changes\" name=\"submit\">";
+
 
     } else {
         echo "Thall shall not pass!";
