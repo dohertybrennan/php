c7e0e3e9dd42d7a339c52a62f5b53db64126ed5e
Brennan Doherty <dohertybrennan@yahoo.com>
2018-10-08 23:14:08 -0400

    test

--- cookies.php
+++ cookies.php
@@ -2,7 +2,7 @@
     $cookie_name = "user";
     $cookie_date = date("l jS \of F Y h:i:s A");
     setcookie($cookie_name, $cookie_date, time()+(84600 * 30), "/");
-    setcookie("cookieSeconds", date_create(), time()+(84600 * 30), "/");
+    setcookie("cookieSeconds", time(), time()+(84600 * 30), "/");
 ?>
 
 <!DOCTYPE html>
