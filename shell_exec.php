bcace92bc9220d7ad232dc903e08f6deadd5a09a
dohertybrennan <dohertybrennan@yahoo.com>
2018-10-10 11:19:59 -0400

    added dir test

--- shell_exec.php
+++ shell_exec.php
@@ -4,4 +4,16 @@
 
     $pwd = shell_exec("pwd");
     echo "<pre>" . $pwd . "</pre>";
+
+    //Check to see if directory exists
+
+    $dir = "test";
+    $ls = shell_exec($dir);
+    $fail = "ls: cannot access '$dir': No such file or directory";
+
+    if ($ls == $fail) {
+        echo "This directory does not exist";
+    } else {
+        echo "this directory does exist";
+    }
 ?>
