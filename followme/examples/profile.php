71deb19fd4daf19b9c575102e6ff6e5d8151643a
dohertybrennan <dohertybrennan@yahoo.com>
2018-10-31 09:30:27 -0400

    test

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -24,6 +24,7 @@ $max_users = $conn->query($sql);
 $sql = "select count(follower) from fm_followers where user_id = $user_id";
 $num_followers = $conn->query($sql);
 
+
 var_dump($num_followers);
 echo("<br><br>");
 var_dump($following_result);
