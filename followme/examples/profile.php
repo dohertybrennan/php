c44825850d7c8ee84e88aa9ad4e53290f953b5a0
dohertybrennan <dohertybrennan@yahoo.com>
2018-10-31 09:29:11 -0400

    test

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -22,11 +22,7 @@ $sql = "SELECT MAX(user_id) FROM fm_users";
 $max_users = $conn->query($sql);
 
 $sql = "select count(follower) from fm_followers where user_id = $user_id";
-$count_followers = $conn->query($sql);
-
-while ($row = $num_followers->fetch_assoc()) {
-    $num_followers[] = $count_followers['count(follower)'];
-}
+$num_followers = $conn->query($sql);
 
 var_dump($num_followers);
 echo("<br><br>");
