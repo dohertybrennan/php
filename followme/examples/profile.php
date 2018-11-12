10386f5e0fccaef57b8e0469db0a94663ac4db62
dohertybrennan <dohertybrennan@yahoo.com>
2018-11-12 11:03:11 -0500

    test

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -87,6 +87,9 @@ while ($row = $following_result->fetch_assoc()) {
 	                    <a href="login.php" class="nav-link">Login</a>
                     </li>
                     <li class="nav-item">
+	                    <a href="followers.php" class="nav-link">Follow Your Nose!</a>
+                    </li>
+                    <li class="nav-item">
 	                    <a href="#" class="nav-link">
                             <?php 
                                 echo $_SESSION['username'];
